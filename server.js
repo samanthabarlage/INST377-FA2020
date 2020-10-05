c33ef2df6603bfd30cb2c6a824717713cf060923
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-10-05 12:38:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,10 +16,15 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
     console.log('POST request detected');
+<<<<<<< HEAD
     res.send('Hello World')
+=======
+    console.log('Form data in res.body', req.body);
+>>>>>>> 25aea25efc2b2c47c2a185b743f71a181e731fb6
   });
 
 app.listen(port, () => {
