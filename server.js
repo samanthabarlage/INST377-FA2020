2c6a43c1a5a2d09e13b5f7fb7b7750d0e4afbae6
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-10-19 18:59:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.send('Hello World')
+    res.json(countries);
   });
 
 app.listen(port, () => {
