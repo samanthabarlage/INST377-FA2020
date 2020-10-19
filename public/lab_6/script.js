e146481de8fd3a604bc1ac921464817fa4416d0c
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-10-19 19:25:31 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -31,6 +31,7 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
       console.log('fromServer', fromServer);
+      console.log(range(243));
     })
     .catch((err) => console.log(err));
 });
