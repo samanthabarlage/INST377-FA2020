074ece0fea70653363cf2fd8e54b6a6c0686082f
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-10-20 23:40:39 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -51,8 +51,8 @@ document.body.addEventListener('submit', async (e) => {
 
       reverseList.forEach((el, i) => {
         const li = document.createElement('li');
-        $(li).append('<input type="checkbox" value=${el.code} id=${el.code} />');
-        $(li).append('<label for=${el.code}> ${el.name}</label>');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}> ${el.name}</label>`);
         $(li).append(li);
       });
     })
