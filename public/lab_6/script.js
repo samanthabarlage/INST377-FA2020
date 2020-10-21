cc98ef99c1a030a91459703fe4f1811bdcdb3149
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-10-20 23:10:33 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -54,6 +54,7 @@ document.body.addEventListener('submit', async (e) => {
         $(li).append('<label for=${el.code}> ${el.name}</label>');
         $(li).append(li);
       });
+      console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
 });
