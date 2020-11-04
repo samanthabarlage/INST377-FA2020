ffd0b69b3898c03f71b44ddbba9f5aa828b9803f
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-11-04 11:38:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-const json = await data.json();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,11 +26,13 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     //res.json(countries);
-    console.log(json);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json);
     res.json(json);
   });
 
